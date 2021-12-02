7893d17b849d677757a707e049d8afc39546c062
叶健新 <yejx38448@hundsun.com>
2021-12-02 11:16:15 +0800

    feat: 追加写入

---
+++ src/c/cProject/1-6-追加写入/test.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#pragma warning(disable:4996)
+
+const char* str = "\nhello c";
+
+int main() {
+	FILE* fp = NULL;
+
+	fp = fopen("../test1.txt", "a+");
+
+	if (!fp) {
+		perror("File Open Failed!");
+		exit(EXIT_FAILURE);
+	}
+
+	const char ret = fputs(str, fp);
+
+	if (!ret) {
+		exit(EXIT_FAILURE);
+	}
+
+	fclose(fp);
+
+	return 0;
+}
