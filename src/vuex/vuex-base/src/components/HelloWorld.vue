04ab8cef3f6266134b479ab54d643d02c764b449
叶健新 <yejx38448@hundsun.com>
2021-12-02 23:08:15 +0800

    feat: vuex 使用

---
+++ src/vuex/vuex-base/src/components/HelloWorld.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>{{ count }}</p>
+    <p>getter: {{ countlag10 }}</p>
+    <button @click="increase(1)">增加</button>
+    <button @click="decrease(1)">减少</button>
+  </div>
+</template>
+
+<script>
+import { mapState, mapGetters, mapMutations, mapActions } from 'vuex';
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+  computed: {
+    ...mapState({
+      count: state => state.count
+    }),
+    ...mapGetters({
+      countlag10: 'countlag10'
+    })
+  },
+  methods: {
+    ...mapMutations(['increase']),
+    ...mapActions(['decrease'])
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
