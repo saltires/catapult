5bd3b695f373c7fb9f0d0f86b3320d727ad04039
叶健新 <yejx38448@hundsun.com>
2022-03-04 15:17:19 +0800

    feat: update ts

--- src/ts/1-基础篇章/1-20-函数重载.ts
+++ src/ts/1-基础篇章/1-20-函数重载.ts
@@ -17,5 +17,10 @@ makeDate(1)
 makeDate(1, 2, 3)
 
 /**
+ * 前两个 makeDate 是函数签名 并没有函数具体的实现
+ * 最后一个 makeDate 则是对函数具体的实现，里面需要对不同参数情形做判断和处理
+ */
+
+/**
  * 关于函数重载，具体参考 https://www.typescriptlang.org/docs/handbook/2/functions.html#generic-functions
  */
